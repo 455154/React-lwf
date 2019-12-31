58a3056ed08661eea08d785defb7eee666800027
Levi <349345860@qq.com>
2020-01-01 00:44:25 +0800

    init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+// const title = <h1>JSX</h1>
+// ReactDOM.render(title,document.getElementById('root'))
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// function Hello() {
+//   function edit() {
+//     alert("ok");
+//   }
+//   return <div onClick={edit}>first组件</div>;
+// }
+// ReactDOM.render(<Hello />, document.getElementById("root"));
+// ReactDOM.render(Hello(),document.getElementById('root'))
+class Hello extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      count: 0
+    };
+    this.click = this.click.bind(this);
+  }
+  render() {
+    return <div onClick={this.click}>{this.state.count}</div>;
+  }
+  click = () => {
+    this.setState({
+      count: this.state.count + 1
+    });
+  };
+}
+ReactDOM.render(<Hello />, document.getElementById("root"));
